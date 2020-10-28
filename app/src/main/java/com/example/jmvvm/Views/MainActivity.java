681ade79d3bee43169730d67c5a80a434e26b990
tayyabmughal676 <tayyabmughal676@gmail.com>
2020-10-28 12:49:03 +0500

    Menu Cart Item and Set Navigation System.

--- app/src/main/java/com/example/jmvvm/Views/MainActivity.java
+++ app/src/main/java/com/example/jmvvm/Views/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.jmvvm.Views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.jmvvm.R;
 
@@ -19,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
 //        Control Navigation system
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 }
